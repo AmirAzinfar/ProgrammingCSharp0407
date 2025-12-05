62cebf4588b82ed60aec61f8a132420e171e320a
Amir Azinfar <azinfar.amiryahoo.de>
2025-12-05 18:57:04 +0100

    Connected to Database and created new Tables

--- ProgrammingInCshrpBaseBackend/Sevices/JobService.cs
+++ ProgrammingInCshrpBaseBackend/Sevices/JobService.cs
@@ -9,7 +9,7 @@ namespace ProgrammingInCshrpBaseBackend.Services
         private List<Job> jobs = new List<Job> ();
         public void Add(Job job)
         {
-            jobs.Add(job);
+            //jobs.Add(job);
         }
 
         public List<Job> GetAll()
